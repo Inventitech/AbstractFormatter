6d14a41cab2df8886992eea55b66d99ea1f3588d
Thomas Kinnen <thomas.kinnen@gmail.com>
2014-04-28 21:08:51 +0200

    Fix formatting using gjslint and fixjsstyle

--- js/abstractFormatting.js
+++ js/abstractFormatting.js
@@ -15,7 +15,7 @@
 
 
 // General formatting method calling sub-check functions.
-var formatText = function (inputText) {
+var formatText = function(inputText) {
     inputText = removeCommentedOutLines(inputText);
     checkMultipleParagraphs(inputText);
     inputText = removeWhitespaces(inputText);
@@ -28,64 +28,64 @@ var formatText = function (inputText) {
     inputText = checkTeXSyntax(inputText);
 
     return inputText;
-}
+};
 
 
 var removeCommentedOutLines = function(inputText) {
     return inputText.replace(/^%.*$/mg, '');
-}
+};
 
 // checks whether abstract contains multiple paragraphs. If so, gives a warning that they'll be flattened.
-var checkMultipleParagraphs = function (inputText) {
+var checkMultipleParagraphs = function(inputText) {
     var divId = 'multipleParagraphs';
     if (containsMultipleParagraphs(inputText)) {
-        addInfoMessage(divId, 'alert alert-info', "It appears to me, your abstract contains multiple paragraphs. I flattened them.");
+        addInfoMessage(divId, 'alert alert-info', 'It appears to me, your abstract contains multiple paragraphs. I flattened them.');
     } else {
         removeInfoMessage(divId);
     }
-}
+};
 
 // Returns true if abstract has multiple paragraphs, false otherwise.
-var containsMultipleParagraphs = function (inputText) {
+var containsMultipleParagraphs = function(inputText) {
     inputText = inputText.replace(/[\f\t\v\u00A0\u2028\u2029]/gi, '');
     if (inputText.match(/\s{2,}\S+/g) != null) {
         return true;
     }
     return false;
-}
+};
 
 // Removes all whitespaces in the abstract.
-var removeWhitespaces = function (inputText) {
+var removeWhitespaces = function(inputText) {
     // White-space replacements
     inputText = inputText.replace(/\s/gi, ' ');
     inputText = inputText.replace(/\s+/g, ' ');
     inputText = inputText.replace(/(\w+)- (\w+)/g, '$1$2'); // deals with hyphenation: hy- phen -> hyphen
     inputText = inputText.replace(/\s+$/, ''); // remove trailing white spaces
     return inputText;
-}
+};
 
 // Checks the start of the abstract and removes abstract from its beginning.
-var checkAbstractStart = function (inputText) {
-    if(startsWithAbstract(inputText)) {
+var checkAbstractStart = function(inputText) {
+    if (startsWithAbstract(inputText)) {
         inputText = inputText.replace(/^abstract(\W)*/i, '');
     }
     return inputText;
-}
+};
 
 // Returns whether the abstract starts with abstract. If so, adds a warning and returns true (false otherwise).
-var startsWithAbstract = function (inputText) {
+var startsWithAbstract = function(inputText) {
     var divId = 'abstractInfo';
-    if(inputText.match(/^abstract(\W)*/i) != null) {
-        addInfoMessage(divId, 'alert alert-warning', "Your abstract begins with the words abstract. I removed them for you.");
+    if (inputText.match(/^abstract(\W)*/i) != null) {
+        addInfoMessage(divId, 'alert alert-warning', 'Your abstract begins with the words abstract. I removed them for you.');
         return true;
     } else {
         removeInfoMessage(divId);
         return false;
     }
-}
+};
 
 // Checks and returns the number of words in the abstract.
-var checkLength = function (inputText) {
+var checkLength = function(inputText) {
     var divId = 'lengthWarning';
     var wordsInAbstract = inputText.match(/\b/g);
     var numberOfWords = 0;
@@ -95,55 +95,55 @@ var checkLength = function (inputText) {
 
     removeInfoMessage(divId);
     if (numberOfWords > 200 && numberOfWords <= 250) {
-        addInfoMessage(divId, 'alert alert-warning', "Your abstract exceeds 200 words: " + numberOfWords + " words is often considered rather long.");
+        addInfoMessage(divId, 'alert alert-warning', 'Your abstract exceeds 200 words: ' + numberOfWords + ' words is often considered rather long.');
     } else if (numberOfWords > 250 && numberOfWords <= 500) {
-        addInfoMessage(divId, 'alert alert-danger', "Your text exceeds 250 words: " + numberOfWords + " words is often considered too long for an article.");
+        addInfoMessage(divId, 'alert alert-danger', 'Your text exceeds 250 words: ' + numberOfWords + ' words is often considered too long for an article.');
     } else if (numberOfWords > 500) {
-        addInfoMessage(divId, 'alert alert-danger', "Your text exceeds 500 words: " + numberOfWords + " words is <strong>generally considered too long.</strong>");
+        addInfoMessage(divId, 'alert alert-danger', 'Your text exceeds 500 words: ' + numberOfWords + ' words is <strong>generally considered too long.</strong>');
     } else {
         removeInfoMessage(divId);
     }
 
     return numberOfWords;
-}
+};
 
 // Checks whether the abstract ends correctly, with a fullstop, question or exclamation mark.
-var checkParagraphEndsCorrectly = function (inputText, length) {
+var checkParagraphEndsCorrectly = function(inputText, length) {
     var divId = 'paragraphEnd';
     if (inputText.match(/[.?!]$/) === null && length > 3) {
-        addInfoMessage(divId, 'alert alert-danger', "Your last sentence does not end in a fullstop, question or exclamation mark!");
+        addInfoMessage(divId, 'alert alert-danger', 'Your last sentence does not end in a fullstop, question or exclamation mark!');
     } else {
         removeInfoMessage(divId);
     }
-}
+};
 
 // checks whether abstract contains references. If so, gives a warning.
-var checkReferences = function (inputText) {
+var checkReferences = function(inputText) {
     var divId = 'checkReferences';
     if (containsReferences(inputText)) {
-        addInfoMessage(divId, 'alert alert-warning', "Your abstract contains references. It should not do that.");
+        addInfoMessage(divId, 'alert alert-warning', 'Your abstract contains references. It should not do that.');
     } else {
         removeInfoMessage(divId);
     }
-}
+};
 
 // Returns true if the abstract contains references such as [5]
-var containsReferences = function (inputText) {
-    if(inputText.match(/\[(.*)?\d(.*)?\]/g) != null) {
+var containsReferences = function(inputText) {
+    if (inputText.match(/\[(.*)?\d(.*)?\]/g) != null) {
         return true;
     }
-}
+};
 
 // Performs some basic TeX replacements
 var checkTeXSyntax = function(inputText) {
     inputText = inputText.replace(/\\\S+{(.*?)}/gi, '$1'); // replaces TeX commands like \it{Text}
     inputText = inputText.replace(/{\\\S+(.*?)}/gi, '$1'); // replaces TeX commands like {\em Text}
     inputText = inputText.replace(/\\/gi, '');
-    inputText = inputText.replace(/---/g, "&mdash;");
-    inputText = inputText.replace(/--/g, "&ndash;");
+    inputText = inputText.replace(/---/g, '&mdash;');
+    inputText = inputText.replace(/--/g, '&ndash;');
 
     return inputText;
-}
+};
 
 
 
